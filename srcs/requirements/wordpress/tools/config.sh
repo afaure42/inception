1097c5e5b9dc4b3a993e818036d2d0a2ed686fe5
afaure <afaure@student.42.fr>
2022-09-26 10:24:07 -0400

    wordpress container should be working

---
+++ srcs/requirements/wordpress/tools/config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ ! -a "/var/www/wordpress/wp-config.php" ]; then
+	wp config create \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 \
+	--path="/var/www/wordpress" \
+	--allow-root
+
+	wp core install --allow-root \
+					--url="https://afaure.42.fr" \
+					--title="afaure.42.fr" \
+					--admin_user=$WD_ADMIN \
+					--admin_password=$WP_ADMIN_PASSWORD \
+					--path="/var/www/wordpress"
+	wp user create	afaure \
+					afaure@student.42.fr \
+					--user_pass=$WP_USER_PASS \
+					--allow-root \
+					--path="/var/www/wordpress"
+	fi
+
+if [ ! -a /run/php ]; then
+	mkdir /run/php
+	fi
+
+/usr/sbin/php-fpm7.3 -F
