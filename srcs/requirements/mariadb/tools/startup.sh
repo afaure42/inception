d8c3e85db105f1ce9ba86805c86df0f5c860232b
afaure <afaure@student.42.fr>
2022-09-28 12:16:08 -0400

    WIP ON FUCKING MARIADB

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+if [ ! -d /var/lib/mysql/$SQL_DATABASE ]; then
+
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+mysql -e "UPDATE mysql.user SET Password=PASSWORD('$SQL_ROOT_PASSWORD') WHERE User='root';"
+mysql -e "DELETE FROM mysql.user WHERE User='';"
+mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -e "DROP DATABASE test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+
+mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+echo "DATABASE CREATED"
+mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+echo "USER CREATED"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+echo "PERMS GRANTED"
+mysql -u root -e "FLUSH PRIVILEGES;"
+echo "FLUSH"
+echo $SQL_ROOT_PASSWORD
+service mysql status
+
+mysqladmin -uroot shutdown
+echo "STOPPING"
+
+fi
+
+echo "STARTING COMMAND"
+exec "$@"
