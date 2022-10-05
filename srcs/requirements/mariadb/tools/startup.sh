cd285a3eb47dbe5bac5e64f18a904aee0daccce5
afaure <afaure@student.42.fr>
2022-10-05 12:35:19 +0200

    Mariadb ok, no remote connect + password always required

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -4,27 +4,26 @@ if [ ! -d /var/lib/mysql/$SQL_DATABASE ]; then
 
 service mysql start
 
-mysql -e "UPDATE mysql.user SET Password=PASSWORD('$SQL_ROOT_PASSWORD') WHERE User='root';"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -e "DROP DATABASE test;"
-mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
-mysql -e "FLUSH PRIVILEGES;"
+# Secure installation
+mysql <<-EOF
+ALTER USER \`root\`@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+DELETE FROM mysql.user WHERE User='';
+GRANT ALL PRIVILEGES ON *.* TO \`root\`@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+echo "Secure installation done"
 
+#database creation
+mysql -u root -p$SQL_ROOT_PASSWORD <<-EOF
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+echo "Wordpress database and user created"
 
-mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-echo "DATABASE CREATED"
-mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-echo "USER CREATED"
-mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-echo "PERMS GRANTED"
-mysql -u root -e "FLUSH PRIVILEGES;"
-echo "FLUSH"
-echo $SQL_ROOT_PASSWORD
-service mysql status
-
-mysqladmin -uroot shutdown
-echo "STOPPING"
+mysqladmin -uroot -p$SQL_ROOT_PASSWORD shutdown
+echo "STOPPING TEMP SERV"
 
 fi
 
